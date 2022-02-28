992b658ba3f7fef484bbe1afb011b2b2bc004c41
woong-s <jiungdev@gmail.com>
2022-02-28 16:00:12 +0900

    add: init api server

---
+++ board/contract_board/ref/src/index.js
@@ -0,0 +1,6 @@
+import Web3 from "web3";
+
+
+
+
+const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/919227b4a0ce4359a847fdae2cebde84"));
--- swapToken20Or721/src/lib/errors.js
+++
@@ -1,6 +0,0 @@
-const NO_ETHEREUM_OBJECT = /No Ethereum provider was found on window.ethereum/;
-
-export const isNoEthereumObject = (err) => {
-  return NO_ETHEREUM_OBJECT.test(err);
-};
-
